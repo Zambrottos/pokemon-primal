7f443fae3cba11f9482f3e51a16d6d1f64773331
Zambrottos <zambrottos@localhost>
2026-09-22 18:21:10 +0800

    Add persistent rescued starter disappearance

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1406,7 +1406,7 @@
 #define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2D)
 
 #define FLAG_STARTED_VICTINI_QUEST                  (SYSTEM_FLAGS + 0x2E)
-#define FLAG_UNUSED_0x88F                           (SYSTEM_FLAGS + 0x2F) // Unused Flag
+#define FLAG_PRIMAL_HIDE_RESCUED_STARTER            (SYSTEM_FLAGS + 0x2F)
 
 #define FLAG_SYS_TV_HOME                            (SYSTEM_FLAGS + 0x30)
 #define FLAG_SYS_TV_WATCH                           (SYSTEM_FLAGS + 0x31)
